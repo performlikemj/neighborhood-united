343023133ea8164d7553d3f30c712089e6c4f593
MJ <mikey.j.1228@gmail.com>
2023-12-13 12:43:06 +0900

    Remove code for geojson

--- local_chefs/views.py
+++ local_chefs/views.py
@@ -1,10 +1,6 @@
 from django.http import JsonResponse
 from .models import ChefPostalCode, PostalCode
 from chefs.models import Chef
-import json
-import os
-
-from django.db.models import Q
 
 def chef_service_areas(request, query):
     # Normalize the query to match chef's name or identifier
@@ -18,33 +14,20 @@ def chef_service_areas(request, query):
         return JsonResponse({'error': 'No chefs found based on the query provided'}, status=404)
 
     chef_data = []
-    base_path = os.path.join(BASE_DIR, 'static/geojson/NY')  # Adjust the path as necessary
 
     for chef in chefs:
         # Fetch postal codes served by the chef
         postal_codes_served = ChefPostalCode.objects.filter(chef=chef).values_list('postal_code__code', flat=True)
 
-        geojson_features = []
-        for code in postal_codes_served:
-            file_path = os.path.join(base_path, f"{code}.geojson")
-            try:
-                with open(file_path, 'r') as file:
-                    geojson = json.load(file)
-                    geojson_features.extend(geojson['features'])
-            except FileNotFoundError:
-                print(f"GeoJSON file for postal code {code} not found.")
-
         # Add chef information and service areas
         chef_info = {
             "chef_id": chef.id,
             "name": chef.user.username,
-            "service_areas": {"type": "FeatureCollection", "features": geojson_features}
+            "service_areas": list(postal_codes_served)
         }
         chef_data.append(chef_info)
 
     return JsonResponse(chef_data, safe=False)  # safe=False is used as the top-level object is not a dict
-
-
 
 def service_area_chefs(request, query):
     # Normalize the query to match the postal code format
@@ -67,20 +50,9 @@ def service_area_chefs(request, query):
             # Add more chef details as needed
         } for chef_serve in chefs_serving]
 
-        # Load the GeoJSON for the postal code
-        base_path = os.path.join(BASE_DIR, 'static/geojson/NY')  # Adjust the path as necessary
-        file_path = os.path.join(base_path, f"{normalized_query}.geojson")
-        try:
-            with open(file_path, 'r') as file:
-                geojson_features = json.load(file)['features']
-        except FileNotFoundError:
-            print(f"GeoJSON file for postal code {normalized_query} not found.")
-            geojson_features = []
-
         response_data = {
             "postal_code": postal_code.code,
             "chefs": chef_info,
-            "geojson_features": geojson_features
         }
 
         return JsonResponse(response_data)

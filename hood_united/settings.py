b6416ee60db493aff4d72fa09cd89337b4054004
MJ <mikey.j.1228@gmail.com>
2024-01-24 23:52:59 +0900

    Update ALLOWED_HOSTS in settings.py

--- hood_united/settings.py
+++ hood_united/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'nbhd.azurewebsites.net', 'www.nbhdunited.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'nbhd.azurewebsites.net', 'www.nbhdunited.com', 'nbhdunited.com']
 
 
 # Application definition
